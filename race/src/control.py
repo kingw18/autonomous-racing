19a91e92c167b7bebbac27d0046ca0d15f92c347
ou-brandon <bro9tn@virginia.edu>
2022-04-12 17:43:29 -0400

    new code

--- race/src/control.py
+++ race/src/control.py
@@ -11,7 +11,8 @@ ki = 0.0 #TODO
 servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
 prev_error = 0.0
 err_sum = 0
-
+err_queue = [] 
+err_queue_size = 20
  
 # This code can input desired velocity from the user.
 # velocity must be between [0,100] to move forward. 
@@ -24,7 +25,7 @@ vel_input = 0.0	#TODO
 
 # Publisher for moving the car. 
 # TODO: Use the coorect topic /car_x/offboard/command.
-command_pub = rospy.Publisher('/car_9/offboard/command', AckermannDrive, queue_size = 1)
+command_pub = rospy.Publisher('/car_3/offboard/command', AckermannDrive, queue_size = 1)
 
 def control(data):
 	global prev_error
@@ -35,7 +36,8 @@ def control(data):
 	global scalar
 	global err_sum
 	angle = 0.0
-
+	global err_queue
+	global err_queue_size
 	print("PID Control Node is Listening to error")
 	
 	## Your PID code goes here
@@ -43,23 +45,47 @@ def control(data):
 	
 	# 1. Scale the error
 	# err_scaled = data.pid_error*scalar
+	
+	if len(err_queue) >= err_queue_size:
+		err_sum -= err_queue.pop()
+	
 	err_sum += data.pid_error
-
+	err_queue.insert(0, data.pid_error)
+	# err_sum += data.pid_error
+	error = data.pid_error
+	# print("Error:", error)
 	# 2. Apply the PID equation on error to compute steering
 	# thetad = kp*data.pid_error + kd*(prev_error - err_scaled)
 	# for derivative of error, need (current error - prev_error)/time between messages
-	angle = kp*data.pid_error + kd*(prev_error - data.pid_error) + ki*err_sum
-
+	error_diff = prev_error - error
+	angle = kp*data.pid_error + kd*(error_diff) + ki*err_sum
 	
+	# print("Error difference:", error_diff)
+	# print("Angle:", angle)
 	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
 	command = AckermannDrive()
 
 	# TODO: Make sure the steering value is within bounds [-100,100]
+	angle = angle if angle < 100 else 100
+	angle = angle if angle > -100 else -100
 	command.steering_angle = angle
-
-	# TODO: Make sure the velocity is within bounds [0,100]
-	command.speed = vel_input
-
+	
+	
+	if abs(error) > 1.96:
+		set_speed = vel_input*1.4/math.sqrt(abs(error))
+		# print("Scaled speed to:", set_speed)
+	else:
+		set_speed = vel_input
+	if set_speed < 15:
+		set_speed = 15
+	if set_speed > 60:
+		set_speed = 60
+	command.speed = set_speed
+	
+	prev_error=error
+	# print("Error:", data.pid_error)
+	# print("Angle:", angle)
+	# print("Speed:", set_speed)
 	# Move the car autonomously
 	command_pub.publish(command)
 
@@ -72,7 +98,6 @@ if __name__ == '__main__':
 	kd = input("Enter Kd Value: ")
 	ki = input("Enter Ki Value: ")
 	vel_input = input("Enter desired velocity: ")
-	scalar = input("Input error scalar")
 	rospy.init_node('pid_controller', anonymous=True)
 	rospy.Subscriber("error", pid_input, control)
 	rospy.spin()
--- race/src/dist_finder.py
+++ race/src/dist_finder.py
@@ -12,17 +12,25 @@ desired_distance = 0.9	# distance from the wall (in m). (defaults to right wall)
 vel = 15 		# this vel variable is not really used here.
 error = 0.0		# initialize the error
 car_length = 0.50 # Traxxas Rally is 20 inches or 0.5 meters
+car_width = 0.50  # TODO: How wide is the car??
+prev_distance = 0
+forward_angle = 90	# Angle representing the direction the car is currently facing
+min_threschold = 0.05
+# FTG Variables
+gap_threshold = 1.0	# Threshold distance for defining gap
+bubble_rad = 10	# The radius (in angle increments) of the safety bubble (naive implementation)
 
 # Handle to the publisher that will publish on the error topic, messages of the type 'pid_input'
 pub = rospy.Publisher('error', pid_input, queue_size=10)
 
-
+# Lidar's min range is .02 and max range is 6
 def getRange(data, angle):
 	# data: single message from topic /scan
     # angle: between -30 to 210 degrees, where 0 degrees is directly to the right, and 90 degrees is directly in front
     # Outputs length in meters to object with angle in lidar scan field of view
     # Make sure to take care of NaNs etc.
-
+	global prev_distance
+	'''
 	# Convert angle to LIDAR's coordinates
 	angle += 30
 	angle = math.radians(angle)
@@ -34,24 +42,142 @@ def getRange(data, angle):
 	if data.range_min <= distance <= data.range_max:
 		return distance
 	return 100.0	# TODO: Better implementation of NaNs?
+	'''
+	distance = data.ranges[int((angle+30)*len(data.ranges)/240)]
+	if data.range_min <= distance <= data.range_max:
+		prev_distance=distance	
+		return distance
+	return prev_distance
 
 
+def dist_between_measurements(dist1, dist2, angle):
+	"""
+	Computes the distance between two measurement points given their respective distances from the car
+	and the angle between them
+	"""
+	dist_squared = (dist1 ** 2) + (dist2 ** 2) - (2 * dist1 * dist2 * math.cos(math.radians(angle)))	# Law of Cosines
+	return dist_squared ** 0.5
+
+
+def ftg_target_angle(data):
+	"""
+	Returns the target goal determine by Follow-The-Gap in degrees
+	"""
+	angle_increment = len(data.ranges) / angle_range
+
+	# Copy the distance into a list that can be manipulated in order to preserve the original data
+	ranges = []
+	closest_point = data.range_max + 1
+	closest_point_ind = -1
+	for i in range(len(data.ranges)):
+		dist = data.ranges[i]
+		'''
+		if data.range_min <= dist <= data.range_max:
+			ranges.append(data.range_max + 1) # Use as a substitute for infinity, actual value shouldn't matter as long as its greater than max
+		else:
+			if dist < closest_point:
+				closest_point = dist
+				closest_point_ind = i
+			ranges.append(dist)
+'''
+		
+		if dist < closest_point and dist >= min_threschold:
+			closest_point = dist
+			closest_point_ind = i
+			ranges.append(dist)
+		
+		elif dist > data.range_max:
+			ranges.append(data.range_max + 1)
+		
+		elif dist <= min_threschold:
+			# global prev_distance
+			# data.ranges[i] = prev_distance # this is sus so we might need to replace this
+			ranges.append(data.range_max + 1)
+		else:
+			ranges.append(dist)
+	# Create Safety Bubble - Naive Implementation
+	"""
+	This just creates the bubble around a certain number of measurements on either side of the closest point.
+	I remember Prof. Behl saying in lecture that a more proper implementation would actually compute distances
+	to make the radius of the bubble equal to the width of the car. I tried to implement that below
+	"""
+	print('Closest point dist: ' + str(closest_point) + ' at ' + str(closest_point_ind/angle_increment - 30) + ' degrees.')
+	for i in range(max(0, closest_point_ind - bubble_rad), min(len(data.ranges), closest_point_ind + bubble_rad)):
+		ranges[i] = 0
+
+	# # Create Safety Bubble - Full Implementation
+	# """
+	# I think this is the proper implementation but it seems computationally very expensive
+	# """
+	# ind = closest_point_ind + 1
+	# while ind < len(ranges) and dist_between_measurements(closest_point, ranges[ind], angle_increment * (ind - closest_point_ind)) <= car_width:
+	# 	ranges[ind] = 0
+	# 	ind += 1
+	# ind = closest_point_ind - 1
+	# while ind >= 0 and dist_between_measurements(closest_point, ranges[ind], angle_increment * (closest_point_ind - ind)) <= car_width:
+	# 	ranges[ind] = 0
+	# 	ind -= 1
+
+	# TODO: Disparity Extender
+
+	# Find Max Gap
+	max_gap = (0, 0)
+	gap_start = 0
+	in_gap = False
+	for i in range(len(ranges)):
+		if in_gap and ranges[i] == 0:	# Should we compare directly to 0 or within a certain threshold?
+			if i - gap_start > max_gap[1] - max_gap[0]:
+				max_gap = (gap_start, i)
+			in_gap = False
+		elif not in_gap and ranges[i] > 0:
+			gap_start = i
+			in_gap = True
+	
+	if in_gap and len(ranges) - gap_start > max_gap[1] - max_gap[0]:
+		max_gap = (gap_start, len(ranges))
+		
+	# print(max_gap[0], max_gap[1])
+	# max_gap[1]-=1
+	# Find target angle - Naive Implementation (Find deepest point)
+	max_ind = -1
+	deepest_dist = 0
+	for i in range(max_gap[0], max_gap[1]):
+		if ranges[i] > deepest_dist:
+			deepest_dist = ranges[i]
+			max_ind = i
+	
+
+	print('Min: ' + str(max_gap[0]/angle_increment - 30) + ' Max: ' + str(max_gap[1]/angle_increment - 30))
+	print(str(-30 + max_ind/angle_increment) + ' degrees')
+	# return -30 + (max_ind/angle_increment)
+	return ((max_gap[0] + max_gap[1])/2)/angle_increment - 30
+	# print(data.angle_increment)
+	# print(-30 + ((max_gap[0] + max_gap[1])/2)*angle_increment)	
+	# return -30 + ((max_gap[0] + max_gap[1])/2)*data.angle_increment
 def callback(data):
 	global forward_projection
 
-	theta = 70 # you need to try different values for theta
+	"""
+	# Legacy Code
+	theta = 60 # you need to try different values for theta
 	a = getRange(data, theta) # obtain the ray distance for theta
 	b = getRange(data, 0)	# obtain the ray distance for 0 degrees (i.e. directly to the right of the car)
+	print(a, b)
 	theta = math.radians(theta)	# Convert to radians to make angle computations work
 
 	# Compute Alpha, AB, and CD.. and finally the error.
 
 	alpha = math.atan((a * math.cos(theta) - b)/(a * math.sin(theta)))
-	AB = b * math.cos(alpha)
-	AC = vel 
-	CD = AB + AC * math.sin(alpha)
-	error = desired_distance - CD
+	"""
+	alpha = ftg_target_angle(data) - forward_angle
+	# print(alpha+forward_angle)
+	alpha = math.radians(alpha)
 	
+	# AB = b * math.cos(alpha)
+	# AC = vel
+	# CD = AB + AC * math.sin(alpha)
+	error = vel * math.sin(vel * math.sin(alpha))
+	# print(error)	
 
 	msg = pid_input()	# An empty msg is created of the type pid_input
 	# this is the error that you want to send to the PID for steering correction.
