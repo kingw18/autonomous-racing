4f8317a2ba840525672e91717130f33fe460ca08
ou-brandon <bro9tn@virginia.edu>
2022-05-10 15:49:51 -0400

    Fixed pure pursuit

--- race/src/dist_find_pure_pursuit.py
+++ race/src/dist_find_pure_pursuit.py
@@ -17,10 +17,24 @@ car_width = 0.4  # Car is about 30cm wide
 prev_distance = 0
 forward_angle = 90  # Angle representing the direction the car is currently facing
 min_threshold = 0.05
-
-# Pure Pursuit Variables
-waypoints = [(0, 0), (80, 7), (87, -2), (10, -46), (102, -65), (90, -95), (0, -123), (-34, -43)]
-lookahead_dist = 1
+steering_angle_constant = .7
+# ure Pursuit Variables
+# waypoints = [(0, 0), (80, 7), (87, -2), (10, -46), (102, -65), (90, -95), (0, -123), (-34, -43)]
+'''
+waypoints = [(0, 0), (.3, -.08), (.63, -.25), (1, -.4), (1.5, -.63), (1.9, -.86), (2.4, -1), 
+	(2.8, -1.1), (3.17, -1.16), (3.5, -.94), (3.71, -.64), (3.8, -.01), (3.61, .22),
+	(3.19, .62), (2.31, 1.1), (1.82, 1.67), (1.99, 2.01), (2.61, 2.3), (3.14, 2.43),
+	(3.65, 2.55), (4.07, 2.86), (4.29, 3.31), (4.23, 3.86), (3.92, 4.32), (3.61, 4.68),
+	(3.25, 5.06), (2.53, 5.4), (1.9, 5.51), (1.25, 5.5), (.47, 5.22), (-.25, 4.9),
+	(-.78, 4.32), (-1.07, 3.9), (-1.48, 2.72), (-1.35, 2.03), (-1.15, 1.35), (-1.03, .91),(-.78, .46), (-.39, .08)]
+'''
+waypoints = [
+	(-.09, -.01), (.3, -.14), (.61, -.32), (1.04, -.49), (1.45, -.61), (2.07, -.8), (2.9, -.87), (3.49, -.7),
+	(3.78, -.45), (3.84, .16), (3.28, .57), (2.56, .86), (2.04, 1.04), (1.5, 1.5), (1.32, 1.6), (1.32, 2.09),
+	(1.79, 2.61), (2.69, 2.61), (3.39, 2.53), (4.15, 2.4), (4.76, 2.74), (5.1, 3.41), (4.59, 3.91),
+	(3.92, 4.24), (3.1, 4.76), (2.22, 5), (1.27, 5.32), (.32, 5.4), (-.78, 4.56), (-1.45, 3.67), (-1.74, 2.68),
+	(-1.63, 1.87), (-1.19, 1.15), (-.7, .63)]
+lookahead_dist = 1.5
 
 # Global Variables
 car_x = 0
@@ -35,6 +49,7 @@ def quad_formula(a, b, c):
 	"""
 	Helper function to compute the quadratic formula
 	"""
+	print('a, b, c', a, b, c)
 	return (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
 
 
@@ -55,8 +70,8 @@ def steering_angle(y, L):
 	@param L: The lookahead distance
 	@return: the steering angle gamma
 	"""
-	return 2 * abs(y) / (L ** 2)
-
+	# return steering_angle_constant * 2 * abs(y) / (L ** 2)
+	return steering_angle_constant * 2 * y / (L ** 2)
 
 def dist(p1, p2):
 	"""
@@ -75,7 +90,7 @@ def goal_point():
 		if dist(waypoints[i], (car_x, car_y)) < dist_to_waypoint:
 			dist_to_waypoint = dist(waypoints[i], (car_x, car_y))
 			closest_i = i
-
+	
 	small_i	= closest_i	# The index of the last waypoint before the lookahead distance
 	large_i = closest_i	# The index of the first waypoint after the lookahead distance
 	short_dist = dist_to_waypoint	# The distance of the last waypoint before the lookahead distance
@@ -85,7 +100,13 @@ def goal_point():
 		large_i %= len(waypoints)
 		short_dist = long_dist
 		long_dist = dist(waypoints[large_i], (car_x, car_y))
-
+	
+	if short_dist > lookahead_dist:
+		return waypoints[small_i]
+	
+	print('Small i waypoint:', small_i)
+	print('Large i waypoint:', large_i)
+	print('Waypoint small i:', waypoints[small_i])
 	# Lots of math to interpolate between the two waypoints-- not sure if this can be simplified?
 	a = short_dist
 	c = dist(waypoints[large_i], waypoints[small_i])
@@ -112,10 +133,16 @@ def find_steering_angle():
 	Determines the steering angle based on the current inferred position
 	"""
 	goal = goal_point()
+	print('Before Translationg Goal:', goal, 'Car pos:', car_x, car_y)
 	goal = (goal[0] - car_x, goal[1] - car_y)	# Translate origin to car position
+	print('After translation Goal:', goal)
 	# Roatate so that x axis is along the car
-	goal = (goal[0] * math.sin(car_theta) + goal[1] * math.cos(car_theta), goal[0] * math.cos(car_theta) - goal[1] * math.sin(car_theta))
-	return steering_angle(goal[1], lookahead_dist)
+	goal = (goal[0] * math.cos(-1 * car_theta) - goal[1] * math.sin(-1 *car_theta),  (goal[0] * math.sin(-1 * car_theta) + goal[1] * math.cos(-1 * car_theta)))
+	print('After rotation goal:', goal)
+	print('Car theta:', car_theta)
+	angle = steering_angle(goal[1], lookahead_dist)
+	print('Angle:', angle)
+	return angle
 
 
 def callback(data):
@@ -124,7 +151,7 @@ def callback(data):
 
 	# error = vel * math.sin(vel * math.sin(alpha))  # ADD THIS BACK LATER PROBABLY
 	error = vel * alpha
-	print('Error:', error)
+	# print('Error:', error)
 	msg = pid_input()  # An empty msg is created of the type pid_input
 	# this is the error that you want to send to the PID for steering correction.
 	msg.pid_error = error
---
+++ race/src/pure_pursuit_control.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+import math
+import rospy
+from race.msg import pid_input
+from ackermann_msgs.msg import AckermannDrive
+
+# PID Control Params
+kp = 0.0 #TODO
+kd = 0.0 #TODO
+ki = 0.0 #TODO
+servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
+prev_error = 0.0
+err_sum = 0
+err_queue = [] 
+err_queue_size = 10
+min_speed = 25
+max_speed = 50 
+# This code can input desired velocity from the user.
+# velocity must be between [0,100] to move forward. 
+# The following velocity values correspond to different speed profiles.
+# 15: Very Slow (Good for debug mode)
+# 25: Slow and steady
+# 35: Nice Autonomous Pace
+# > 40: Careful, what you do here. Only use this if your autonomous steering is very reliable.
+vel_input = 0.0	#TODO
+
+# Publisher for moving the car. 
+# TODO: Use the coorect topic /car_x/offboard/command.
+command_pub = rospy.Publisher('/car_3/offboard/command', AckermannDrive, queue_size = 1)
+
+def control(data):
+	global prev_error
+	global vel_input
+	global kp
+	global kd
+	global angle
+	global scalar
+	global err_sum
+	angle = 0.0
+	global err_queue
+	global err_queue_size
+	print("PID Control Node is Listening to error")
+	
+	## Your PID code goes here
+	#TODO: Use kp, ki & kd to implement a PID controller
+	
+	# 1. Scale the error
+	# err_scaled = data.pid_error*scalar
+	
+	if len(err_queue) >= err_queue_size:
+		err_sum -= err_queue.pop()
+	
+	err_sum += data.pid_error
+	err_queue.insert(0, data.pid_error)
+	# err_sum += data.pid_error
+	error = data.pid_error
+	front_dist = data.pid_vel
+	# print("Error:", error)
+	# 2. Apply the PID equation on error to compute steering
+	# thetad = kp*data.pid_error + kd*(prev_error - err_scaled)
+	# for derivative of error, need (current error - prev_error)/time between messages
+	error_diff = prev_error - error
+	angle = kp*data.pid_error + kd*(error_diff) + ki*err_sum
+	
+	# print("Error difference:", error_diff)
+	# print("Angle:", angle)
+	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
+	command = AckermannDrive()
+
+	# TODO: Make sure the steering value is within bounds [-100,100]
+	angle = angle if angle < 100 else 100
+	angle = angle if angle > -100 else -100
+	command.steering_angle = angle
+	
+	'''	
+	if front_dist < 1:
+		set_speed = 15
+		# print("Scaled speed to:", set_speed)
+	elif front_dist < 4:
+		set_speed = vel_input/((5-front_dist)**.5)*(8+front_dist)/12
+	else:
+		set_speed = vel_input
+	if set_speed < 15:
+		set_speed = 15
+	if set_speed > 60:
+		set_speed = 60
+	'''
+	# min_speed = 25
+	# max_speed = 80
+	if abs(error) > 1.21:
+		set_speed = vel_input*1.1/math.sqrt(abs(error))
+	else:
+		set_speed = vel_input
+	if set_speed < min_speed:
+		set_speed = min_speed
+	if set_speed > max_speed:
+		set_speed = max_speed
+	command.speed = set_speed
+	
+	prev_error=error
+	# print("Error:", data.pid_error)
+	# print("Angle:", angle)
+	# print("Speed:", set_speed)
+	# Move the car autonomously
+	command_pub.publish(command)
+
+if __name__ == '__main__':
+	# global kp
+	# global kd
+	# global ki
+	# global vel_input
+	# kp = input("Enter Kp Value: ")
+	# kd = input("Enter Kd Value: ")
+	# ki = input("Enter Ki Value: ")
+	# vel_input = input("Enter desired velocity: ")
+	kp = 10
+	kd = .1
+	ki = .001
+	vel_input = max_speed
+	rospy.init_node('pid_controller', anonymous=True)
+	rospy.Subscriber("error", pid_input, control)
+	rospy.spin()
