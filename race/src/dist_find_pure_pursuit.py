d7d61614da2a7ac5d8074bf47e7a12e872e83a7a
ou-brandon <bro9tn@virginia.edu>
2022-05-11 02:04:46 -0400

    backup working code

--- race/src/dist_find_pure_pursuit.py
+++ race/src/dist_find_pure_pursuit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
 import math
 from sensor_msgs.msg import LaserScan
@@ -18,6 +19,8 @@ prev_distance = 0
 forward_angle = 90  # Angle representing the direction the car is currently facing
 min_threshold = 0.05
 steering_angle_constant = .7
+dynamic_lookahead = True
+dynamic_steering_angle = True
 # ure Pursuit Variables
 # waypoints = [(0, 0), (80, 7), (87, -2), (10, -46), (102, -65), (90, -95), (0, -123), (-34, -43)]
 '''
@@ -28,13 +31,41 @@ waypoints = [(0, 0), (.3, -.08), (.63, -.25), (1, -.4), (1.5, -.63), (1.9, -.86)
 	(3.25, 5.06), (2.53, 5.4), (1.9, 5.51), (1.25, 5.5), (.47, 5.22), (-.25, 4.9),
 	(-.78, 4.32), (-1.07, 3.9), (-1.48, 2.72), (-1.35, 2.03), (-1.15, 1.35), (-1.03, .91),(-.78, .46), (-.39, .08)]
 '''
+'''
 waypoints = [
 	(-.09, -.01), (.3, -.14), (.61, -.32), (1.04, -.49), (1.45, -.61), (2.07, -.8), (2.9, -.87), (3.49, -.7),
 	(3.78, -.45), (3.84, .16), (3.28, .57), (2.56, .86), (2.04, 1.04), (1.5, 1.5), (1.32, 1.6), (1.32, 2.09),
 	(1.79, 2.61), (2.69, 2.61), (3.39, 2.53), (4.15, 2.4), (4.76, 2.74), (5.1, 3.41), (4.59, 3.91),
 	(3.92, 4.24), (3.1, 4.76), (2.22, 5), (1.27, 5.32), (.32, 5.4), (-.78, 4.56), (-1.45, 3.67), (-1.74, 2.68),
 	(-1.63, 1.87), (-1.19, 1.15), (-.7, .63)]
-lookahead_dist = 1.5
+'''
+
+cv_ld = 1.25
+s_ld = 2.0
+cv_an = 1
+s_an = 0.5
+
+waypoints = [
+	(0.3, 0, s_ld, s_an), (0.75, -.25, s_ld, s_an), (1.1, -.45, s_ld, s_an), (1.45, -.61, cv_ld, cv_an), (2.07, -.8, cv_ld, cv_an), (2.9, -0.87, cv_ld, cv_an), (3.25, -.7, cv_ld, cv_an), (3.4, -.45, cv_ld, cv_an),
+	(3.4, 0.16, cv_ld, cv_an), (3.25, 0.57, cv_ld, cv_an), (2.8, 0.86, cv_ld, cv_an), (2.2, 1.2, cv_ld, cv_an), (1.9, 1.45, cv_ld, cv_an), (1.8, 1.75, cv_ld, cv_an), (1.9, 2.09, cv_ld, cv_an), (2.3, 2.4, cv_ld, cv_an),
+	(2.69, 2.55, cv_ld, cv_an), (3.39, 2.6, cv_ld, cv_an), (3.9, 2.65, cv_ld, cv_an), (4.4, 2.9, cv_ld, cv_an), (4.5, 3.41, cv_ld, cv_an), (4.4, 3.8, cv_ld, cv_an), (3.92, 4.24, s_ld, s_an), (3.1, 4.62, s_ld, s_an),
+	(2.22, 5, s_ld, s_an), (1.27, 5.2, s_ld, s_an), (0.32, 5, s_ld, s_an), (-.78, 4.56, s_ld, s_an), (-1.45, 3.67, s_ld, s_an), (-1.74, 2.68, s_ld, 0.8), (-1.63, 1.87, s_ld, 0.8), (-1.19, 1.15, s_ld, 0.8),
+	(-.7, .63, s_ld, 0.8)]
+'''
+
+waypoints = []
+# path = "straight_lines.csv"
+path = sys.argv[1]
+print(path)
+with open(path) as wp_file:
+	for line in wp_file:
+		line = line.strip('\xef\xbb\xbf')
+		points = line.split(',')
+		if len(points) > 1:
+			waypoints.append((float(points[0].strip()), float(points[1].strip()), float(points[2].strip()), float(points[3].strip())))
+
+'''
+lookahead_dist = 1.25
 
 # Global Variables
 car_x = 0
@@ -49,7 +80,7 @@ def quad_formula(a, b, c):
 	"""
 	Helper function to compute the quadratic formula
 	"""
-	print('a, b, c', a, b, c)
+	# print('a, b, c', a, b, c)
 	return (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
 
 
@@ -81,6 +112,7 @@ def dist(p1, p2):
 
 
 def goal_point():
+	global lookahead_dist, steering_angle_constant
 	"""
 	Returns the current goal point based on the pure pursuit slides
 	"""
@@ -95,18 +127,22 @@ def goal_point():
 	large_i = closest_i	# The index of the first waypoint after the lookahead distance
 	short_dist = dist_to_waypoint	# The distance of the last waypoint before the lookahead distance
 	long_dist = dist_to_waypoint	# The distance of the first waypoint after the lookahead distance
+	if dynamic_lookahead:
+		lookahead_dist = waypoints[closest_i][2]
+	if dynamic_steering_angle:
+		steering_angle_constant = waypoints[closest_i][3]
 	while dist(waypoints[large_i], (car_x, car_y)) < lookahead_dist:
 		large_i += 1
 		large_i %= len(waypoints)
 		short_dist = long_dist
 		long_dist = dist(waypoints[large_i], (car_x, car_y))
-	
+		
 	if short_dist > lookahead_dist:
 		return waypoints[small_i]
 	
-	print('Small i waypoint:', small_i)
-	print('Large i waypoint:', large_i)
-	print('Waypoint small i:', waypoints[small_i])
+	# print('Small i waypoint:', small_i)
+	# print('Large i waypoint:', large_i)
+	# print('Waypoint small i:', waypoints[small_i])
 	# Lots of math to interpolate between the two waypoints-- not sure if this can be simplified?
 	a = short_dist
 	c = dist(waypoints[large_i], waypoints[small_i])
@@ -135,13 +171,13 @@ def find_steering_angle():
 	goal = goal_point()
 	print('Before Translationg Goal:', goal, 'Car pos:', car_x, car_y)
 	goal = (goal[0] - car_x, goal[1] - car_y)	# Translate origin to car position
-	print('After translation Goal:', goal)
+	# print('After translation Goal:', goal)
 	# Roatate so that x axis is along the car
 	goal = (goal[0] * math.cos(-1 * car_theta) - goal[1] * math.sin(-1 *car_theta),  (goal[0] * math.sin(-1 * car_theta) + goal[1] * math.cos(-1 * car_theta)))
-	print('After rotation goal:', goal)
-	print('Car theta:', car_theta)
+	# print('After rotation goal:', goal)
+	# print('Car theta:', car_theta)
 	angle = steering_angle(goal[1], lookahead_dist)
-	print('Angle:', angle)
+	# print('Angle:', angle)
 	return angle
 
 
